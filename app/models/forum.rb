5ed7e9d14cfbefda9e3f57ac44cfb08924c1542c
tatatatanita <y_shibata_nagoya@yahoo.co.jp>
2020-01-14 20:54:11 +0900

    comment-form

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -2,13 +2,12 @@ class Forum < ApplicationRecord
   has_many :forum_users
   has_many :users, through: :forum_users
   has_many :posts
-  
   validates :name, presence: true, uniqueness: true
 
   def show_last_post
     if (last_post = posts.last).present?
-      if last_post.content?
-        last_post.content
+      if last_post.text?
+        last_post.text
       else
         '画像が投稿されています'
       end
@@ -16,5 +15,4 @@ class Forum < ApplicationRecord
       'まだメッセージはありません。'
     end
   end
-
 end
